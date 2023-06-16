6aeb8125e6a710838dca45923a5005ce25fa1aee
Abhishek Singh <66420195+web-abhi@users.noreply.github.com>
2023-06-17 01:25:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
     print("You're eligible for rollercoaster ride!")
     age = int(input("What is your age in years? "))
-    if age >= 18:
-        print("You have to pay $20.")
-    elif age >= 12 < 18:
-        print("You have to pay $12.")
-    else:
+    if age <= 12:
         print("You have to pay $7.")
+    elif age >= 18:
+        print("You have to pay $20.")
+    else:
+        print("You have to pay $12.")
 else:
     print("Sorry, you're under age for rollercoaster.")
